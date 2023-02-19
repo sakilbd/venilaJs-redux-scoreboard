9d2bd05484e893ba531feffb6467103ca9e04893
unknown <mdsakiluzzaman@gmail.com>
2023-02-19 21:49:21 +0600

    trying to update states dynamically

--- scripts.js
+++ scripts.js
@@ -1,44 +1,19 @@
 // select dom elements
 
 const c = console.log.bind(console);
-const counterEl = document.getElementById("counter");
-const incrementEl = document.getElementById("increment");
-const decrementEl = document.getElementById("decrement");
-const resetBtn = document.getElementById("reset");
+const counterEl = document.getElementById("counter1");
+const incrementEl = document.getElementById("increment1");
+const decrementEl = document.getElementById("decrement1");
+const resetBtn = document.getElementById("reset1");
+
+const matchEl = document.getElementById("match1");
 const containerEl = document.getElementById("container");
-const matchEl = document.getElementById("match");
+const matchCreateBtn = document.getElementById("match-create-button");
 
-const query = document.querySelectorAll(".match >.wrapper >.lws-matchName")[0]
-    .innerHTML;
+// const query = document.querySelectorAll(".match >.wrapper >.lws-matchName")[0]
+//     .innerHTML;
 
-// query.forEach((userItem) => {
-//     c(userItem);
-// });
-
-c(query);
-
-let i = 0;
-const cloneNode = () => {
-    const cloneMathEl = matchEl.cloneNode(true);
-    cloneMathEl.id = "match" + ++i;
-    containerEl.appendChild(cloneMathEl);
-
-
-    const incrementInput = document.querySelectorAll("#match1 #increment")[0];
-    const decrementInput = document.querySelectorAll("#match1 #decrement")[0];
-
-    incrementInput.id = "increment" + i;
-    decrementInput.id = "decrement" + i;
-    c(incrementInput);
-    c(decrementInput);
-};
-// const cloneMathEl = matchEl.cloneNode(true);
-// cloneMathEl.id = "match1";
-// containerEl.appendChild(cloneMathEl);
-
-cloneNode();
-
-
+// c(query);
 
 // action identifiers
 const INCREMENT = "increment";
@@ -139,4 +114,64 @@ resetBtn.addEventListener("click", () => {
     incrementEl.value = "";
     decrementEl.value = "";
     store.dispatch(reset());
+});
+
+let i = 2;
+const cloneNode = () => {
+    const cloneMathEl = matchEl.cloneNode(true);
+    cloneMathEl.id = "match" + i;
+    containerEl.appendChild(cloneMathEl);
+
+    const incrementInput = document.querySelectorAll(`#match${i} #increment1`)[0];
+    const decrementInput = document.querySelectorAll(`#match${i} #decrement1`)[0];
+    const result = document.querySelectorAll(`#match${i} #counter1`)[0];
+    const closeBtn = document.querySelectorAll(`#match${i} #close1`)[0];
+
+    incrementInput.id = "increment" + i;
+    decrementInput.id = "decrement" + i;
+    result.id = "result" + i;
+    closeBtn.id = "close" + i;
+    // c(incrementInput);
+    // c(decrementInput);
+    // c(result);
+    // c(closeBtn);
+    // for (let idx = 1; idx <= i; idx++) {
+    //     window["counterEl_" + idx] = document.getElementById(`counter${idx}`);
+    //     window["incrementEl_" + idx] = document.getElementById(`increment${idx}`);
+    //     window["decrementEl_" + idx] = document.getElementById(`decrement${idx}`);
+    //     window["resetBtn_" + idx] = document.getElementById(`reset${idx}`);
+    //     window["matchEl_" + idx] = document.getElementById(`match${idx}`);
+
+    //     let incrementDynamicId = `incrementEl_${i}`;
+    //     let decrementDynamicID = `decrementEl_${i}`;
+    //     // let resetDynamicId = `resetBtn_${i}`;
+    //     incrementDynamicId.onkeydown = function(e) {
+    //         if (e.key == "Enter") {
+    //             const value = incrementDynamicId.value;
+    //             store.dispatch(increment(parseInt(value ? value : 0)));
+    //             e.preventDefault();
+    //         }
+    //     };
+    //     decrementDynamicID.onkeydown = function(e) {
+    //         if (e.key == "Enter") {
+    //             const value = decrementDynamicID.value;
+    //             store.dispatch(decrement(parseInt(value ? value : 0)));
+    //             e.preventDefault();
+    //         }
+    //     };
+    // }
+
+    // resetDynamicId.addEventListener("click", () => {
+    //     incrementDynamicId.value = "";
+    //     decrementDynamicID.value = "";
+    //     store.dispatch(reset());
+    // });
+};
+// const cloneMathEl = matchEl.cloneNode(true);
+// cloneMathEl.id = "match1";
+// containerEl.appendChild(cloneMathEl);
+
+matchCreateBtn.addEventListener("click", () => {
+    cloneNode();
+    ++i;
 });
